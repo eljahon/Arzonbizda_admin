22870b2e20b880254331ea6bdcb9f9402fc2bebf
eljahon <neljahon@gmail.com>
2022-05-20 20:09:37 +0500

    assesg

--- src/views/profile/components/Account.vue
+++ src/views/profile/components/Account.vue
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 <template>
   <el-form>
     <el-row>
@@ -40,8 +39,6 @@
     </el-row>
   </el-form>
 </template>
-
->>>>>>> 1781230 (user info get to backand)
 <script>
 import Upload from '../../components-demo/avatar-upload'
 import { userPersolnData } from '@/api/userInfo'
@@ -80,105 +77,3 @@ export default {
 }
 </script>
 
-<template>
-  <el-form>
-    <el-row>
-      <el-col><Upload /></el-col>
-      <el-col :lg="10">
-        <el-row>
-          <el-col :lg="24">
-            <el-form-item class="form__label__style" label="Имя">
-              <el-input v-model.trim="user.email" class="accaunt__input" />
-            </el-form-item>
-          </el-col>
-          <el-col :lg="24">
-            <el-form-item label="Фамилия" class="form__label__style">
-              <el-input v-model.trim="user.email" class="accaunt__input" />
-            </el-form-item>
-          </el-col>
-          <el-col :lg="24">
-            <el-form-item
-              class="form__label__style"
-              label="Адрес электронной почты"
-            >
-              <el-input v-model.trim="user.email" class="accaunt__input" />
-            </el-form-item>
-          </el-col>
-          <el-col :lg="24">
-            <el-row>
-              <el-form-item>
-                <div class="buttuns-warapper">
-                  <el-col :span="16" class="btn__full">
-                    <el-button
-                      class="profile__setting btn__full"
-                      icon="el-icon-document"
-                      type="primary"
-                      @click="submit"
-                    >Сохранить изменения</el-button>
-                  </el-col>
-                  <el-col :lg="8" class="btn__box btn__revert">
-                    <el-button
-                      class="profile__setting btn__full"
-                      style="float: right"
-                      icon="el-icon-circle-close"
-                      type="danger"
-                    >Отмена</el-button>
-                  </el-col>
-                </div>
-              </el-form-item>
-            </el-row>
-          </el-col>
-        </el-row>
-      </el-col>
-    </el-row>
-  </el-form>
-</template>
-
-<style scoped lang="scss">
-.buttuns-warapper {
-  display: flex;
-  justify-content: space-between;
-}
-.btn__box {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.profile__setting__btn {
-  font-family: "SF Pro";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 16px;
-}
-.accaunt__input {
-  font-family: "Yandex Sans Display";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 40px;
-  color: #242625;
-}
-.form__label__style {
-  font-family: "Yandex Sans Display";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 32px;
-  color: #4a4d4d;
-}
-@media (max-width: 433px) {
-  .buttuns-warapper {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  .btn__revert {
-    margin-top: 10px;
-  }
-  .btn__full {
-    width: 100%;
-  }
-}
-</style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -53,66 +53,8 @@ export default {
         email: 'admin@test.com',
         avatar: this.avatar
       }
-    },
-    computed: {
-      ...mapGetters([
-        'name',
-        'avatar',
-        'roles'
-      ])
     }
   }
 }
 </script>
-<template>
-  <div class="app-container">
-    <div v-if="user">
-      <el-row :gutter="20">
-        <!--                <el-col :span="6" :xs="24">-->
-        <!--                  <user-card :user="user" />-->
-        <!--                </el-col>-->
 
-        <el-col :span="24" :xs="24">
-          <el-card>
-            <el-tabs v-model="activeTab">
-              <el-tab-pane
-                class="tab__section__style"
-                label="Информация об аккаунте"
-                name="Информация об аккаунте"
-              >
-                <account class="prifil-setting" :user="user" />
-              </el-tab-pane>
-              <el-tab-pane
-                class="tab__section__style"
-                label="Информация о безопасности"
-                name="Информация о безопасности"
-              >
-                <ProfileSetting class="prifil-setting" />
-              </el-tab-pane>
-            </el-tabs>
-          </el-card>
-        </el-col>
-      </el-row>
-    </div>
-  </div>
-</template>
-<style scoped lang="scss">
-.prifil-setting {
-  margin-top: 60px;
-  margin-left: 60px;
-}
-.tab__section__style {
-  font-family: "Yandex Sans Display";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 24px;
-  color: #242625;
-}
-@media (max-width: 624px) {
-  .prifil-setting {
-    margin-top: 20px;
-    margin-left: 0;
-  }
-}
-</style>
